ccc48eb0559972538ec5517308f5dc9812b82090
Odin Rumianowski <103415989+ORumianowski@users.noreply.github.com>
2024-08-01 17:04:49 +0200

    Add files via upload

---
+++ 02_models/SEIR_with_sea.R
@@ -0,0 +1,219 @@
+
+
+
+# -------------------------------------------------------------------------
+
+# transition A to sea
+
+# -------------------------------------------------------------------------
+
+
+
+library(tidyverse)
+library(ggplot2)
+library(reshape2)
+library(abind)
+library(cowplot)
+
+# Parameters
+total_time <- 160          # Simulation time
+
+sigma <- 1/5.2             # Rate of progression from exposed to infectious (inverse of incubation period)
+gamma <- 1/2.9             # Recovery rate (inverse of infectious period)
+eta <-  (1/5.2)*(0.1)*50   # Rate of progression from infectious to exposed
+mu <- (1/2.9) * 0.5        # Rate of mortality 
+
+beta_colony <- 0.01        # Transmission rate in a colony
+beta_sea <- 0.001          # Transmission rate at sea
+beta = c(beta_colony, beta_sea)
+
+
+tau = 0.001                # Tansition from colony A to the sea
+
+# Initial state
+
+## In colony A
+N <- 1000                  # Total population in colony A
+initial_infected <- 1
+
+initial_exposed <- 0
+initial_recovered <- 0
+initial_susceptible <- N - initial_infected - initial_exposed - initial_recovered
+initial_dead <- 0
+
+initial_state_A <- c(S = initial_susceptible,
+                     E = initial_exposed,
+                     I = initial_infected,
+                     R = initial_recovered,
+                     D = initial_dead)
+
+## At sea
+N <- 50                  # Total population at sea
+initial_infected <- 0
+
+initial_exposed <- 0
+initial_recovered <- 0
+initial_susceptible <- N - initial_infected - initial_exposed - initial_recovered
+initial_dead <- 0
+
+initial_state_sea <- c(S = initial_susceptible,
+                       E = initial_exposed,
+                       I = initial_infected,
+                       R = initial_recovered,
+                       D = initial_dead)
+
+
+
+initial_state = matrix(data = c(initial_state_A,
+                                initial_state_sea), 
+                       nrow = 2, ncol = 5, 
+                       byrow = T)
+
+# Gillespie SEIR model function
+gillespie_seir <- function(N, beta, sigma, gamma, initial_state, total_time) {
+  times <- c(0)
+  states <- array(dim = c(2,5,1), data = initial_state)
+  
+  while (times[length(times)] < total_time) {
+    
+    S_a <- states[1, 1, dim(states)[3]]
+    E_a <- states[1, 2, dim(states)[3]]
+    I_a <- states[1, 3, dim(states)[3]]
+    R_a <- states[1, 4, dim(states)[3]]
+    D_a <- states[1, 5, dim(states)[3]]
+    
+    S_sea <- states[2, 1, dim(states)[3]]
+    E_sea <- states[2, 2, dim(states)[3]]
+    I_sea <- states[2, 3, dim(states)[3]]
+    R_sea <- states[2, 4, dim(states)[3]]
+    D_sea <- states[2, 5, dim(states)[3]]
+    
+    rates <- c(
+      "S_a_to_E_a" = beta[1] * S_a * I_a,
+      "E_a_to_S_a" = eta * E_a,
+      "E_a_to_I_a" = sigma * E_a,
+      "I_a_to_R_a" = gamma * I_a,
+      "I_a_to_D_a" = mu * I_a,
+      
+      "S_sea_to_E_sea" = beta[2] * S_sea * I_sea,
+      "E_sea_to_S_sea" = eta * E_sea,
+      "E_sea_to_I_sea" = sigma * E_sea,
+      "I_sea_to_R_sea" = gamma * I_sea,
+      "I_sea_to_D_sea" = mu * I_sea,
+      
+      "S_a_to_S_sea" = tau * S_a,
+      "E_a_to_E_sea" = tau * E_a,
+      "I_a_to_I_sea" = tau * I_a,
+      "R_a_to_R_sea" = tau * R_a
+      
+      
+    )
+    
+    total_rate <- sum(rates)
+    
+    if (total_rate == 0) {
+      break
+    }
+    
+    time_step <- rexp(1, total_rate)
+    times <- c(times, times[length(times)] + time_step)
+    
+    transition <- sample(names(rates), 1, prob = rates / total_rate)
+    
+    if (transition == "S_a_to_E_a") {
+      S_a <- S_a - 1
+      E_a <- E_a + 1
+    } else if (transition == "E_a_to_S_a") {
+      E_a <- E_a - 1
+      S_a <- S_a + 1
+    } else if (transition == "E_a_to_I_a") {
+      E_a <- E_a - 1
+      I_a <- I_a + 1
+    } else if (transition == "I_a_to_R_a") {
+      I_a <- I_a - 1
+      R_a <- R_a + 1
+    } else if (transition == "I_a_to_D_a") {
+      I_a <- I_a - 1
+      D_a <- D_a + 1
+    } else if (transition == "S_sea_to_E_sea") {
+      S_sea <- S_sea - 1
+      E_sea <- E_sea + 1
+    } else if (transition == "E_sea_to_S_sea") {
+      E_sea <- E_sea - 1
+      S_sea <- S_sea + 1
+    } else if (transition == "E_sea_to_I_sea") {
+      E_sea <- E_sea - 1
+      I_sea <- I_sea + 1
+    } else if (transition == "I_sea_to_R_sea") {
+      I_sea <- I_sea - 1
+      R_sea <- R_sea + 1
+    } else if (transition == "I_sea_to_D_sea") {
+      I_sea <- I_sea - 1
+      D_sea <- D_sea + 1
+    } else if (transition == "S_a_to_S_sea") {
+      S_a <- S_a - 1
+      S_sea <- S_sea + 1
+    } else if (transition == "E_a_to_E_sea") {
+      E_a <- E_a - 1
+      E_sea <- E_sea + 1
+    } else if (transition == "I_a_to_I_sea") {
+      I_a <- I_a - 1
+      I_sea <- I_sea + 1
+    } else if (transition == "R_a_to_R_sea") {
+      R_a <- R_a - 1
+      R_sea <- R_sea + 1
+    }
+    
+    
+    new_state = matrix(data = c(S_a, E_a, I_a, R_a, D_a,
+                                S_sea, E_sea, I_sea, R_sea, D_sea), 
+                           nrow = 2, ncol = 5, 
+                           byrow = T)
+    
+    states <- abind(states, new_state)
+  }
+  
+  return(list(times = times, states = states))
+}
+
+
+
+# Run simulation
+result <- gillespie_seir(N, beta, sigma, gamma, initial_state, total_time)
+
+# Convert results to data frame for plotting
+output <- data.frame(time = result$times,
+                     
+                     S_a = result$states[1, 1,],
+                     E_a = result$states[1, 2,],
+                     I_a = result$states[1, 3,],
+                     R_a = result$states[1, 4,],
+                     D_a = result$states[1, 5,],
+                     
+                     S_sea = result$states[2, 1,],
+                     E_sea = result$states[2, 2,],
+                     I_sea = result$states[2, 3,],
+                     R_sea = result$states[2, 4,],
+                     D_sea = result$states[2, 5,])
+
+output_long <- melt(output, id = "time")
+
+# Plot results
+
+output_a = output_long %>% subset(., variable %in% c("S_a", "E_a", "I_a", "R_a", "D_a"))
+plot_a = ggplot(output_a, aes(x = time, y = value, color = variable)) +
+  geom_line() +
+  labs(x = "Time", y = "Number of individuals", color = "Compartment") +
+  theme_minimal() +
+  ggtitle("Stochastic SEIR Model Simulation (Gillespie Algorithm)")
+
+output_sea = output_long %>% subset(., variable %in% c("S_sea", "E_sea", "I_sea", "R_sea", "D_sea"))
+plot_sea = ggplot(output_sea, aes(x = time, y = value, color = variable)) +
+  geom_line() +
+  labs(x = "Time", y = "Number of individuals", color = "Compartment") +
+  theme_minimal() +
+  ggtitle("Stochastic SEIR Model Simulation (Gillespie Algorithm)")
+
+
+plot_a 
+plot_sea
