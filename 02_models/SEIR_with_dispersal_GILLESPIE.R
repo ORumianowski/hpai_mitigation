62b85a6583c9b4560b77c041cd0e6f6973195820
Odin <odin.rumianowski@agroparistech.fr>
2024-08-05 15:07:34 +0200

    SEIR_dipsersal_gillespie with dispersal at date T

--- 02_models/SEIR_with_dispersal_GILLESPIE.R
+++ 02_models/SEIR_with_dispersal_GILLESPIE.R
@@ -18,7 +18,7 @@ library(cowplot)
 
 # Simulation time
 
-total_time <- 50    
+total_time = 50    
 
 # Epidemiological parameters
 
@@ -58,40 +58,40 @@ disp_param = list(
 # Initial state
 
 ## In colony A
-N <- 200                  
-initial_infected <- 1
-initial_exposed <- 0
-initial_recovered <- 0
-initial_susceptible <- N - initial_infected - initial_exposed - initial_recovered
-initial_dead <- 0
+N = 200                  
+initial_infected = 1
+initial_exposed = 0
+initial_recovered = 0
+initial_susceptible = N - initial_infected - initial_exposed - initial_recovered
+initial_dead = 0
 
-initial_state_A <- c(S = initial_susceptible,
+initial_state_A = c(S = initial_susceptible,
                      E = initial_exposed,
                      I = initial_infected,
                      R = initial_recovered,
                      D = initial_dead)
 
 ## At sea
-N <- 50                  
-initial_infected <- 0
-initial_exposed <- 0
-initial_recovered <- 0
-initial_susceptible <- N - initial_infected - initial_exposed - initial_recovered
-initial_dead <- 0
+N = 50                  
+initial_infected = 0
+initial_exposed = 0
+initial_recovered = 0
+initial_susceptible = N - initial_infected - initial_exposed - initial_recovered
+initial_dead = 0
 
-initial_state_sea <- c(S = initial_susceptible,
+initial_state_sea = c(S = initial_susceptible,
                        E = initial_exposed,
                        I = initial_infected,
                        R = initial_recovered,
                        D = initial_dead)
 
 ## In colony B
-N <- 200                  
-initial_infected <- 0
-initial_exposed <- 0
-initial_recovered <- 0
-initial_susceptible <- N - initial_infected - initial_exposed - initial_recovered
-initial_dead <- 0
+N = 200                  
+initial_infected = 0
+initial_exposed = 0
+initial_recovered = 0
+initial_susceptible = N - initial_infected - initial_exposed - initial_recovered
+initial_dead = 0
 
 initial_state_B <- c(S = initial_susceptible,
                      E = initial_exposed,
@@ -106,8 +106,9 @@ initial_state = matrix(data = c(initial_state_A,
                        nrow = 3, ncol = 5, 
                        byrow = T)
 
+
 # Gillespie SEIR model function
-gillespie_seir <- function(epi_param, 
+gillespie_seir = function(epi_param, 
                            disp_param,
                            initial_state, 
                            total_time) {
@@ -126,38 +127,75 @@ gillespie_seir <- function(epi_param,
   dispersal_date = disp_param$dispersal_date
   
   # Initialization
-  times <- c(0)
-  states <- array(dim = c(3,5,1), data = initial_state)
+  times = c(0)
+  states = array(dim = c(3,5,1), data = initial_state)
   already_dispersed = F
   
   # Next event
   while (times[length(times)] < total_time) {
     
-    S_a <- states[1, 1, dim(states)[3]]
-    E_a <- states[1, 2, dim(states)[3]]
-    I_a <- states[1, 3, dim(states)[3]]
-    R_a <- states[1, 4, dim(states)[3]]
-    D_a <- states[1, 5, dim(states)[3]]
+    S_a = states[1, 1, dim(states)[3]]
+    E_a = states[1, 2, dim(states)[3]]
+    I_a = states[1, 3, dim(states)[3]]
+    R_a = states[1, 4, dim(states)[3]]
+    D_a = states[1, 5, dim(states)[3]]
     
-    S_sea <- states[2, 1, dim(states)[3]]
-    E_sea <- states[2, 2, dim(states)[3]]
-    I_sea <- states[2, 3, dim(states)[3]]
-    R_sea <- states[2, 4, dim(states)[3]]
-    D_sea <- states[2, 5, dim(states)[3]]
+    S_sea = states[2, 1, dim(states)[3]]
+    E_sea = states[2, 2, dim(states)[3]]
+    I_sea = states[2, 3, dim(states)[3]]
+    R_sea = states[2, 4, dim(states)[3]]
+    D_sea = states[2, 5, dim(states)[3]]
     
-    S_b <- states[3, 1, dim(states)[3]]
-    E_b <- states[3, 2, dim(states)[3]]
-    I_b <- states[3, 3, dim(states)[3]]
-    R_b <- states[3, 4, dim(states)[3]]
-    D_b <- states[3, 5, dim(states)[3]]
+    S_b = states[3, 1, dim(states)[3]]
+    E_b = states[3, 2, dim(states)[3]]
+    I_b = states[3, 3, dim(states)[3]]
+    R_b = states[3, 4, dim(states)[3]]
+    D_b = states[3, 5, dim(states)[3]]
     
     # Transmission period
     period = findInterval(times[length(times)], transmission_period) 
     beta_colony_t = beta_colony[period]
     beta_sea_t = beta_sea[period]
     
+    # Rates of each possible event
+    rates = c(
+      "S_a_to_E_a" = beta_colony_t * S_a * I_a,
+      "E_a_to_S_a" = eta * E_a,
+      "E_a_to_I_a" = sigma * E_a,
+      "I_a_to_R_a" = gamma * I_a,
+      "I_a_to_D_a" = mu * I_a,
+      
+      "S_sea_to_E_sea" = beta_sea_t * S_sea * I_sea,
+      "E_sea_to_S_sea" = eta * E_sea,
+      "E_sea_to_I_sea" = sigma * E_sea,
+      "I_sea_to_R_sea" = gamma * I_sea,
+      "I_sea_to_D_sea" = mu * I_sea,
+      
+      "S_b_to_E_b" = beta_colony_t * S_b * I_b,
+      "E_b_to_S_b" = eta * E_b,
+      "E_b_to_I_b" = sigma * E_b,
+      "I_b_to_R_b" = gamma * I_b,
+      "I_b_to_D_b" = mu * I_b,
+      
+      "S_a_to_S_sea" = zeta * S_a,
+      "E_a_to_E_sea" = zeta * E_a,
+      "I_a_to_I_sea" = zeta * I_a,
+      "R_a_to_R_sea" = zeta * R_a
+      
+      
+    )
+    
+    total_rate = sum(rates)
+    
+    if (total_rate == 0) {
+      break
+    }
+    
+    time_step = rexp(1, total_rate)
+    next_time = times[length(times)] + time_step
+    
     # Induction of dispersion
-    if (round(times[length(times)] ) == dispersal_date & !already_dispersed) { 
+    if (next_time > dispersal_date & !already_dispersed) { 
       
       # Number of adults in colony A
       N_a = S_a + E_a + I_a + R_a
@@ -201,105 +239,118 @@ gillespie_seir <- function(epi_param,
       R_b = R_b + disp_R_a_prospecting
       
       already_dispersed = T
+      
+      #########
+      
+      #times = c(times, dispersal_date)
+      
+      # Transmission period
+      period = findInterval(times[length(times)], transmission_period) 
+      beta_colony_t = beta_colony[period]
+      beta_sea_t = beta_sea[period]
+      
+      # Rates of each possible event
+      rates = c(
+        "S_a_to_E_a" = beta_colony_t * S_a * I_a,
+        "E_a_to_S_a" = eta * E_a,
+        "E_a_to_I_a" = sigma * E_a,
+        "I_a_to_R_a" = gamma * I_a,
+        "I_a_to_D_a" = mu * I_a,
+        
+        "S_sea_to_E_sea" = beta_sea_t * S_sea * I_sea,
+        "E_sea_to_S_sea" = eta * E_sea,
+        "E_sea_to_I_sea" = sigma * E_sea,
+        "I_sea_to_R_sea" = gamma * I_sea,
+        "I_sea_to_D_sea" = mu * I_sea,
+        
+        "S_b_to_E_b" = beta_colony_t * S_b * I_b,
+        "E_b_to_S_b" = eta * E_b,
+        "E_b_to_I_b" = sigma * E_b,
+        "I_b_to_R_b" = gamma * I_b,
+        "I_b_to_D_b" = mu * I_b,
+        
+        "S_a_to_S_sea" = zeta * S_a,
+        "E_a_to_E_sea" = zeta * E_a,
+        "I_a_to_I_sea" = zeta * I_a,
+        "R_a_to_R_sea" = zeta * R_a
+        
+        #########
+        
+      )
+      
+      total_rate = sum(rates)
+      
+      if (total_rate == 0) {
+        break
+      }
+      
+      time_step = rexp(1, total_rate)
+      next_time = times[length(times)] + time_step
         
     }
     
-    # Rates of each possible event
-    rates <- c(
-      "S_a_to_E_a" = beta_colony_t * S_a * I_a,
-      "E_a_to_S_a" = eta * E_a,
-      "E_a_to_I_a" = sigma * E_a,
-      "I_a_to_R_a" = gamma * I_a,
-      "I_a_to_D_a" = mu * I_a,
-      
-      "S_sea_to_E_sea" = beta_sea_t * S_sea * I_sea,
-      "E_sea_to_S_sea" = eta * E_sea,
-      "E_sea_to_I_sea" = sigma * E_sea,
-      "I_sea_to_R_sea" = gamma * I_sea,
-      "I_sea_to_D_sea" = mu * I_sea,
-      
-      "S_b_to_E_b" = beta_colony_t * S_b * I_b,
-      "E_b_to_S_b" = eta * E_b,
-      "E_b_to_I_b" = sigma * E_b,
-      "I_b_to_R_b" = gamma * I_b,
-      "I_b_to_D_b" = mu * I_b,
-      
-      "S_a_to_S_sea" = zeta * S_a,
-      "E_a_to_E_sea" = zeta * E_a,
-      "I_a_to_I_sea" = zeta * I_a,
-      "R_a_to_R_sea" = zeta * R_a
-      
-      
-    )
+
+    times = c(times, next_time)
     
-    total_rate <- sum(rates)
-    
-    if (total_rate == 0) {
-      break
-    }
-    
-    time_step <- rexp(1, total_rate)
-    times <- c(times, times[length(times)] + time_step)
-    
-    transition <- sample(names(rates), 1, prob = rates / total_rate)
+    transition = sample(names(rates), 1, prob = rates / total_rate)
     
     if (transition == "S_a_to_E_a") {
-      S_a <- S_a - 1
-      E_a <- E_a + 1
+      S_a = S_a - 1
+      E_a = E_a + 1
     } else if (transition == "E_a_to_S_a") {
-      E_a <- E_a - 1
-      S_a <- S_a + 1
+      E_a = E_a - 1
+      S_a = S_a + 1
     } else if (transition == "E_a_to_I_a") {
-      E_a <- E_a - 1
-      I_a <- I_a + 1
+      E_a = E_a - 1
+      I_a = I_a + 1
     } else if (transition == "I_a_to_R_a") {
-      I_a <- I_a - 1
-      R_a <- R_a + 1
+      I_a = I_a - 1
+      R_a = R_a + 1
     } else if (transition == "I_a_to_D_a") {
-      I_a <- I_a - 1
-      D_a <- D_a + 1
+      I_a = I_a - 1
+      D_a = D_a + 1
     } else if (transition == "S_sea_to_E_sea") {
-      S_sea <- S_sea - 1
-      E_sea <- E_sea + 1
+      S_sea = S_sea - 1
+      E_sea = E_sea + 1
     } else if (transition == "E_sea_to_S_sea") {
-      E_sea <- E_sea - 1
-      S_sea <- S_sea + 1
+      E_sea = E_sea - 1
+      S_sea = S_sea + 1
     } else if (transition == "E_sea_to_I_sea") {
-      E_sea <- E_sea - 1
-      I_sea <- I_sea + 1
+      E_sea = E_sea - 1
+      I_sea = I_sea + 1
     } else if (transition == "I_sea_to_R_sea") {
-      I_sea <- I_sea - 1
-      R_sea <- R_sea + 1
+      I_sea = I_sea - 1
+      R_sea = R_sea + 1
     } else if (transition == "I_sea_to_D_sea") {
-      I_sea <- I_sea - 1
-      D_sea <- D_sea + 1
+      I_sea = I_sea - 1
+      D_sea = D_sea + 1
     } else if (transition == "S_b_to_E_b") {
-      S_b <- S_b - 1
-      E_b <- E_b + 1
+      S_b = S_b - 1
+      E_b = E_b + 1
     } else if (transition == "E_b_to_S_b") {
-      E_b <- E_b - 1
-      S_b <- S_b + 1
+      E_b = E_b - 1
+      S_b = S_b + 1
     } else if (transition == "E_b_to_I_b") {
-      E_b <- E_b - 1
-      I_b <- I_b + 1
+      E_b = E_b - 1
+      I_b = I_b + 1
     } else if (transition == "I_b_to_R_b") {
-      I_b <- I_b - 1
-      R_b <- R_b + 1
+      I_b = I_b - 1
+      R_b = R_b + 1
     } else if (transition == "I_b_to_D_b") {
-      I_b <- I_b - 1
-      D_b <- D_b + 1
+      I_b = I_b - 1
+      D_b = D_b + 1
     } else if (transition == "S_a_to_S_sea") {
-      S_a <- S_a - 1
-      S_sea <- S_sea + 1
+      S_a = S_a - 1
+      S_sea = S_sea + 1
     } else if (transition == "E_a_to_E_sea") {
-      E_a <- E_a - 1
-      E_sea <- E_sea + 1
+      E_a = E_a - 1
+      E_sea = E_sea + 1
     } else if (transition == "I_a_to_I_sea") {
-      I_a <- I_a - 1
-      I_sea <- I_sea + 1
+      I_a = I_a - 1
+      I_sea = I_sea + 1
     } else if (transition == "R_a_to_R_sea") {
-      R_a <- R_a - 1
-      R_sea <- R_sea + 1
+      R_a = R_a - 1
+      R_sea = R_sea + 1
     }
     
     
@@ -309,11 +360,11 @@ gillespie_seir <- function(epi_param,
                            nrow = 3, ncol = 5, 
                            byrow = T)
     
-    states <- abind(states, new_state)
+    states = abind(states, new_state)
 
   }
   
-  output <- data.frame(
+  output = data.frame(
     time = times,
     S_a = states[1, 1, ],
     E_a = states[1, 2, ],
@@ -337,7 +388,7 @@ gillespie_seir <- function(epi_param,
 
 # Run simulation
 
-output <- gillespie_seir(epi_param, 
+output = gillespie_seir(epi_param, 
                          disp_param,
                          initial_state, 
                          total_time)
@@ -345,10 +396,10 @@ output <- gillespie_seir(epi_param,
 
 
 # Plot results
-output_long <- melt(output[1:nrow(output)-1, ], id = "time")
+output_long = melt(output[1:nrow(output)-1, ], id = "time")
 
-output_a <- output_long %>% filter(variable %in% c("S_a", "E_a", "I_a", "R_a", "D_a"))
-plot_a <- ggplot(output_a, aes(x = time, y = value, color = variable)) +
+output_a = output_long %>% filter(variable %in% c("S_a", "E_a", "I_a", "R_a", "D_a"))
+plot_a = ggplot(output_a, aes(x = time, y = value, color = variable)) +
   geom_line() +
   labs(x = "Time", y = "Number of individuals", color = "Compartment") +
   theme_minimal() +
@@ -356,8 +407,8 @@ plot_a <- ggplot(output_a, aes(x = time, y = value, color = variable)) +
   scale_color_brewer(palette="Set2")
 
 
-output_sea <- output_long %>% filter(variable %in% c("S_sea", "E_sea", "I_sea", "R_sea", "D_sea"))
-plot_sea <- ggplot(output_sea, aes(x = time, y = value, color = variable)) +
+output_sea = output_long %>% filter(variable %in% c("S_sea", "E_sea", "I_sea", "R_sea", "D_sea"))
+plot_sea = ggplot(output_sea, aes(x = time, y = value, color = variable)) +
   geom_line() +
   labs(x = "Time", y = "Number of individuals", color = "Compartment") +
   theme_minimal() +
@@ -365,8 +416,8 @@ plot_sea <- ggplot(output_sea, aes(x = time, y = value, color = variable)) +
   scale_color_brewer(palette="Set2")
 
 
-output_b <- output_long %>% filter(variable %in% c("S_b", "E_b", "I_b", "R_b", "D_b"))
-plot_b <- ggplot(output_b, aes(x = time, y = value, color = variable)) +
+output_b = output_long %>% filter(variable %in% c("S_b", "E_b", "I_b", "R_b", "D_b"))
+plot_b = ggplot(output_b, aes(x = time, y = value, color = variable)) +
   geom_line() +
   labs(x = "Time", y = "Number of individuals", color = "Compartment") +
   theme_minimal() +
@@ -419,7 +470,7 @@ plot_b
 #   output = gillespie_seir(epi_param, initial_state, total_time)
 #   output_long = melt(output, id = "time")
 #   
-#   output_long_i <- cbind(output_long,
+#   output_long_i = cbind(output_long,
 #                          data.frame(simulation = rep(i, times = nrow(output_long))))
 #   
 #   output_long_list = rbind(output_long_list, output_long_i)
@@ -428,7 +479,7 @@ plot_b
 # }
 # 
 # 
-# output_a <- output_long_list %>% filter(variable %in% c("S_a", "E_a", "I_a", "R_a", "D_a"))
+# output_a = output_long_list %>% filter(variable %in% c("S_a", "E_a", "I_a", "R_a", "D_a"))
 # 
 # p = ggplot()
 # for(i in 1:nb_iterations){
@@ -444,7 +495,7 @@ plot_b
 # p
 # 
 # 
-# data_long <- pivot_longer(response_list, cols = -N_a, names_to = "variable", values_to = "value")
+# data_long = pivot_longer(response_list, cols = -N_a, names_to = "variable", values_to = "value")
 # 
 # # Créer les diagrammes en violon pour chaque variable
 # ggplot(data_long %>% subset(., variable %in% c("dead_a", "max_infected_a")),
