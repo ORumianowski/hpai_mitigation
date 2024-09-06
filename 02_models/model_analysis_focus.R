7ad43ed3baf989f5d38a903bce91578736898d47
Odin <odin.rumianowski@agroparistech.fr>
2024-09-06 16:42:26 -0400

    begining heatmaps

--- 02_models/model_analysis_focus.R
+++ 02_models/model_analysis_focus.R
@@ -1,12 +1,10 @@
 
-
+setwd("C:/Data/1_Adminitratif/Emploi/cornell/02_projet/hpai_mitigation/02_models")
 
 #
 
-# une foonction pour chaque variable réponse
-# plot les trois
-
-# plus d 'itérations
+# variation du temps de réaction - RS
+# variation du nombre d'infecté - P2
 
 #
 
@@ -24,7 +22,7 @@ source("model.R")
 
 # stat_model --------------------------------------------------------------
 
-nb_iterations = 50
+nb_iterations = 3
 #nb_iterations = 25
 
 stat_model = function(nb_iterations_ = nb_iterations,
@@ -72,9 +70,6 @@ stat_model = function(nb_iterations_ = nb_iterations,
   }
   return(response_list)
 }
-
-# res = stat_model()
-# res$nb_infected_colonies
 
 
 
@@ -236,7 +231,7 @@ scenario_plot3 = function(dt,
                  dotsize = 4,
                  fill = "grey",
                  color = "white",
-                 stackratio=0.025)+
+                 stackratio=0.05)+
     geom_violin(data = dt_panel_5 %>% subset(., scenario %in% c("Baseline outbreak")), 
                 aes(x = scenario_short, y = !!sym(column_to_plot)),
                 fill = "grey",
@@ -251,7 +246,7 @@ scenario_plot3 = function(dt,
                             >sc_mean["proactive_strategy",paste0(column_to_plot, "_mean")]) "lightgreen"
                  else "darksalmon",
                  color = "white",
-                 stackratio=0.025)+
+                 stackratio=0.05)+
     geom_violin(data = dt_panel_5 %>% subset(., scenario %in% c("Proactive strategy - Too late")), 
                 aes(x = scenario_short, y = !!sym(column_to_plot)),
                 fill = if (sc_mean["baseline_outbreak",paste0(column_to_plot, "_mean")]
@@ -273,20 +268,20 @@ scenario_plot3 = function(dt,
                  binaxis='y', stackdir='center',
                  dotsize = its_dotsize,
                  color = "darkgrey", alpha = 0.5,
-                 stackratio=0.125)+
+                 stackratio=0.25)+
     geom_dotplot(data = dt_panel_5 %>% subset(., !(scenario %in% c("Proactive strategy - Too late"))), 
                  aes(x = scenario_short, y = !!sym(column_to_plot)),
                  binaxis='y', stackdir='center',
                  dotsize = its_dotsize,
                  color = "darkgrey", alpha = 0.5,
-                 stackratio=0.345)+
+                 stackratio=0.45)+
     geom_dotplot(data =  dt_panel_5 %>% subset(., scenario %in% c("Healty site", "Proactive strategy")), 
                  aes(x = scenario_short, y = !!sym(column_to_plot)),
                  binaxis='y', stackdir='center',
                  dotsize = its_dotsize,
                  fill = "antiquewhite4",
                  color = "antiquewhite4",
-                 stackratio=0.035)+
+                 stackratio=0.05)+
     ggthemes::theme_clean() +
     theme(
       axis.text.x = element_text(angle = 45, hjust = 1),
@@ -308,53 +303,138 @@ scenario_plot3 = function(dt,
 
 
 
+dt_ = scenario_dt(beta_context = 0.5,
+                  time_at_sea_NB_context = 4,
+                  reaching.repro.prob = 0.5)
+
+
+scenario_plot3(dt = dt_, BETA = 0.5, MOB=4, REPRO=0.5,
+               column_to_plot = "equi.lost.survi.ad",
+               index_label = "ENLSA",
+               its_dotsize = 0.6)
+
+
+
+# scratch -----------------------------------------------------------------
 
 
 
 
-
-beta_bank = c(0.02, 0.05, 0.5, 2)
-mob_bank = c(2, 8, 40)
-repro_bank = c(0.4) # c(0.3, 0.5, 0.7)
-
-
-plot_ENLSA <- list()
-plot_infec.col <- list()
-plot_infected_X_time <- list()
-
-i = 0
-
-for (beta in beta_bank){
-  for (mob in mob_bank){
-    for (repro in repro_bank){
-      
-      i = i + 1
-      
-      dt_ = scenario_dt(beta_context = beta,
-                        time_at_sea_NB_context = mob,
-                        reaching.repro.prob = repro)
-      
-      plot_ENLSA[[i]] <- scenario_plot3(dt = dt_, BETA = beta, MOB=mob, REPRO=repro,
-                     column_to_plot = "equi.lost.survi.ad",
-                     index_label = "ENLSA",
-                     its_dotsize = 0.6)
-      
-      plot_infec.col[[i]] <- scenario_plot3(dt = dt_, BETA = beta, MOB=mob, REPRO=repro,
-                                   column_to_plot = "nb_infected_colonies",
-                                   index_label = "Infected colonies",
-                                   its_dotsize = 1.1)
-      
-      plot_infected_X_time[[i]] <- scenario_plot3(dt = dt_, BETA = beta, MOB=mob, REPRO=repro,
-                                            column_to_plot = "infected_X_time",
-                                            index_label = "Infected X Time colonies",
-                                            its_dotsize = 1.1)
-
-      
-    }
+variation_dt = function(beta_context = 0.2,
+                       time_at_sea_NB_context = 4,
+                       reaching.repro.prob = 0.5,
+                       tau = 0.1)
+  {
+  
+  dispersal_reaction_time_bank = 1:3
+  nb_adults_equi_bank = c()
+  nb_infected_colonies_bank = c()
+  infected_X_time_bank = c()
+  
+  for (i in 1:length(bank)){
+    
+    res = stat_model(induced_dispersal_ = T,
+               initial_number_infected_breeders_A_ = 1,
+               dispersal_stochactic_ = T,
+               dispersal_reaction_time_ = dispersal_reaction_time_bank[2],
+               tau_ = tau,
+               BETA_ =  beta_context,  
+               TIME_AT_SEA_NB_ = time_at_sea_NB_context,
+               reaching.repro.prob_ = reaching.repro.prob)
+    
+    nb_adults_equi_bank = c(nb_adults_equi_bank, res$nb_adults_equi)
+    nb_infected_colonies_bank = c(nb_infected_colonies_bank, res$nb_infected_colonies)
+    infected_X_time_bank = c(infected_X_time_bank, res$infected_X_time)
   }
+  
+  
+  
+  dt = 
+    data.frame(
+      scenario = rep(dispersal_reaction_time_bank,
+                     each = length(nb_adults_equi_bank)/length(dispersal_reaction_time_bank)),
+      
+      equi.lost.survi.ad = nb_adults_equi_bank,
+      nb_infected_colonies = nb_infected_colonies_bank,
+      infected_X_time = infected_X_time_bank,
+      
+      
+      
+    )
+  return(dt)
+  
 }
 
-all_plot_ENLSA = grid.arrange(grobs = plot_ENLSA, ncol = 3, nrow = 4)
-all_plot_infec.col = grid.arrange(grobs = plot_infec.col, ncol = 3, nrow = 4)
-all_plot_infected_X_time = grid.arrange(grobs = plot_infected_X_time, ncol = 3, nrow = 4)
+dt = variation_dt(beta_context = 0.2,
+                        time_at_sea_NB_context = 4,
+                        reaching.repro.prob = 0.5,
+                        tau = 0.1)
+
+variation_plot = function(dt,
+                          BETA,
+                          MOB,
+                          REPRO,
+                          column_to_plot,
+                          index_label,
+                          its_dotsize = 0.5){  
+                                           
+  
+  
+  #sc_mean = scenario_mean(dt)
+  
+  dt = dt %>% 
+    mutate(scenario = as.factor(scenario))
+
+  p = ggplot()+
+    geom_violin(data = dt, 
+                aes(x = scenario, y = !!sym(column_to_plot)),
+                fill = "azure3",
+                color = "white",
+                scale = "width",
+                trim=FALSE, position=position_dodge(1)) +
+    geom_dotplot(data =  dt, 
+                 aes(x = scenario, y = !!sym(column_to_plot)),
+                 binaxis='y', stackdir='center',
+                 dotsize = 1,
+                 fill = "black",
+                 color = "grey",
+                 stackratio=0.5)+
+    ggthemes::theme_clean() +
+    theme(
+      #axis.text.x = element_text(angle = 45, hjust = 1),
+      panel.border = element_blank(),
+      axis.title = element_text(size = 11),
+      axis.text = element_text(size = 10),
+      axis.line = element_line(linewidth = 2),
+      panel.background = element_rect(fill = "transparent", color = NA),
+      plot.background = element_rect(fill = "transparent", color = NA),
+      legend.position =  "none"
+    )+
+    ylim(0, NA)+
+    scale_y_log10()+
+    labs(
+         # x = paste0("beta = ", as.character(BETA),
+         #            "  mob = ", as.character(MOB),
+         #            "  repro = ", as.character(REPRO)),
+         y = index_label)  #
+  print(p)
+}
+
+variation_plot(dt,
+               BETA = 0.5,
+               MO = 5 ,
+               REPRO = 5,
+               column_to_plot = "equi.lost.survi.ad",
+               index_label = "ENLSA",
+               its_dotsize = 0.5)
+
+variation_plot(dt,
+               BETA = 0.5,
+               MO = 5 ,
+               REPRO = 5,
+               column_to_plot = "nb_infected_colonies",
+               index_label = "Infected colonies",
+               its_dotsize = 0.5)
+                          
+
 
